80d71f0ec2a3ef506501da58279f6a1432d13634
Henrique Bezerra Dos Santos <henriquebs1601@gmail.com>
2025-03-10 19:58:48 -0300

    mysql

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { , validateArgs } from 'firebase/data-connect';
-
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'projetofinanceiro',
-  location: 'us-central1'
-};
