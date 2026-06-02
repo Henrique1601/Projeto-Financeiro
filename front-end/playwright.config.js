1f2b87c0877665b7e7fedc40387f04d28ac27872
Henrique Bezerra Dos Santos <henriquebs1601@gmail.com>
2026-06-02 12:10:20 -0300

    fix: add playwright webServer config for e2e tests

--- front-end/playwright.config.js
+++ front-end/playwright.config.js
@@ -9,4 +9,10 @@ export default defineConfig({
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
   },
+  webServer: {
+    command: 'npx vite --port 5173',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+    cwd: './',
+  },
 });
